33ca7717750d74a52430dcde7ed37951c18c91ca
Brandon Torrens <btorr067@fiu.edu>
2023-10-13 19:27:14 -0400

    - Adding functionality to check if the table exists in the database. (not finished)
    Signed-off-by: Brandon Torrens <btorr067@fiu.edu>

--- Team28ALTF4Project/Controllers/book_details_controller.cs
+++ Team28ALTF4Project/Controllers/book_details_controller.cs
@@ -17,6 +17,70 @@ namespace Team28BookDetails.Controllers
             _configuration = configuration;
         }
 
+        [HttpPost("CheckifBook_DetailsTableExists")]
+        public JsonResult Post()
+        {
+
+            
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                string tableName = "book_details";
+                string query = $"SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '{mycon.Database}' AND table_name = '{tableName}'";
+
+
+                mycon.Open();
+                try {
+                    using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                    {
+                        int count = Convert.ToInt32(myCommand.ExecuteScalar());
+
+                        if (count > 0)
+                        {
+                            return new JsonResult("Table Exists");
+                        }
+
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+                    }
+                } 
+                catch {
+                    string newQuery = $"CREATE TABLE '{tableName}' (    " +
+                        $"ISBN int NOT NULL," +
+                        $"BookName varchar(200)," +
+                        $"BookDescription varchar(1000)," +
+                        $"Price decimal(4,2)," +
+                        $"Author varchar(150)," +
+                        $"Genre varchar(100)," +
+                        $"Publisher varchar(150)," +
+                        $"YearPublished int(255)," +
+                        $"CopiesSold int(255)," +
+                        $"PRIMARY KEY (ISBN));'";
+
+                    using (MySqlCommand myNewCommand = new MySqlCommand(newQuery, mycon))
+                    {
+                        myNewCommand.ExecuteNonQuery();
+
+                        myReader = myNewCommand.ExecuteReader();
+                        table.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+
+                        return new JsonResult("Table Doesn't Exist, it has been created.");
+                    }
+                }
+
+                return new JsonResult("Connection was never established");
+            }
+        }
+
         [HttpPost("PostBookDetails")]
         public JsonResult Post(Book_Details bookDetails)
         {
