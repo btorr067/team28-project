0ff05206114fa98386fa45599d98b2fbe8647c18
atash005 <146021645+atash005@users.noreply.github.com>
2023-11-01 13:21:50 -0500

    wishlist_controller.cs
    Controller for wishlist

--- Team28ALTF4Project/Controllers/wishlist_controller.cs
+++ Team28ALTF4Project/Controllers/wishlist_controller.cs
@@ -3,39 +3,74 @@
 [Route("api/wishlist")]
 public class WishlistController : ControllerBase
 {
-    private readonly ApplicationDbContext _context;
+    // Inject your data context here
+    private readonly DataContext _context;
 
-    public WishlistController(ApplicationDbContext context)
+    public WishlistController(DataContext context)
     {
         _context = context;
     }
 
+    // Create a new wishlist for a user
     [HttpPost]
-    public async Task<IActionResult> CreateWishlist(Wishlist wishlist)
+    public IActionResult CreateWishlist([FromBody] Wishlist wishlist)
     {
         _context.Wishlists.Add(wishlist);
-        await _context.SaveChangesAsync();
-        return Ok(wishlist);
+        _context.SaveChanges();
+        return Ok();
     }
 
-    [HttpGet("{userId}")]
-    public IActionResult GetWishlists(string userId)
-    {
-        var wishlists = _context.Wishlists.Where(w => w.UserId == userId).ToList();
-        return Ok(wishlists);
-    }
-
+    // Add a book to a user's wishlist
     [HttpPost("{wishlistId}/addBook")]
-    public async Task<IActionResult> AddBookToWishlist(int wishlistId, Book book)
+    public IActionResult AddBookToWishlist(int wishlistId, [FromBody] int bookId)
     {
-        var wishlist = _context.Wishlists.Include(w => w.Books).FirstOrDefault(w => w.Id == wishlistId);
+        var wishlist = _context.Wishlists.Include(w => w.Books).FirstOrDefault(w => w.WishlistId == wishlistId);
         if (wishlist == null)
         {
-            return NotFound();
+            return NotFound("Wishlist not found");
+        }
+
+        var book = _context.Books.Find(bookId);
+        if (book == null)
+        {
+            return NotFound("Book not found");
         }
 
         wishlist.Books.Add(book);
-        await _context.SaveChangesAsync();
-        return Ok(wishlist);
+        _context.SaveChanges();
+        return Ok();
+    }
+
+    // Remove a book from a user's wishlist
+    [HttpDelete("{wishlistId}/removeBook")]
+    public IActionResult RemoveBookFromWishlist(int wishlistId, [FromBody] int bookId)
+    {
+        var wishlist = _context.Wishlists.Include(w => w.Books).FirstOrDefault(w => w.WishlistId == wishlistId);
+        if (wishlist == null)
+        {
+            return NotFound("Wishlist not found");
+        }
+
+        var book = wishlist.Books.FirstOrDefault(b => b.BookId == bookId);
+        if (book != null)
+        {
+            wishlist.Books.Remove(book);
+            _context.SaveChanges();
+        }
+
+        return Ok();
+    }
+
+    // List the books in a user's wishlist
+    [HttpGet("{wishlistId}/books")]
+    public IActionResult GetWishlistBooks(int wishlistId)
+    {
+        var wishlist = _context.Wishlists.Include(w => w.Books).FirstOrDefault(w => w.WishlistId == wishlistId);
+        if (wishlist == null)
+        {
+            return NotFound("Wishlist not found");
+        }
+
+        return Ok(wishlist.Books);
     }
 }
